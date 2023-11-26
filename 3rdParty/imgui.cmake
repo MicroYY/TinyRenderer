b0ea5f387f7372fdb5d2bf9e1ca9f9de4491e4c9
MicroYY <jianhuyy@hotmail.com>
2023-11-26 16:36:07 +0800

    integrate imgui and dx12 as backend. Run imgui demo.

--- 3rdParty/imgui.cmake
+++ 3rdParty/imgui.cmake
@@ -1,17 +1,29 @@
-set(imgui_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/imgui)
+set(imgui_source_dir
+    ${CMAKE_CURRENT_SOURCE_DIR}/imgui
+)
+set(imgui_backend_dir
+    ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends
+)
 
-file(GLOB imgui_sources CONFIGURE_DEPENDS  "${imgui_SOURCE_DIR_}/*.cpp")
-#file(GLOB imgui_impl CONFIGURE_DEPENDS  
-#"${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.cpp" 
-#"${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.h"
-#"${imgui_SOURCE_DIR_}/backends/imgui_impl_vulkan.cpp" 
-#"${imgui_SOURCE_DIR_}/backends/imgui_impl_vulkan.h"
-#)
-add_library(imgui STATIC 
-    ${imgui_sources} ${imgui_impl})
-target_include_directories(imgui PUBLIC $<BUILD_INTERFACE:${imgui_SOURCE_DIR_}>)
-target_include_directories(imgui PUBLIC $<BUILD_INTERFACE:${vulkan_include}>)
-target_link_libraries(imgui PUBLIC glfw ${vulkan_lib})
+file(GLOB imgui_sources
+    CONFIGURE_DEPENDS "${imgui_source_dir}/*.cpp"
+)
+file(GLOB imgui_impl_dx12
+    CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_dx12.cpp"
+    CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_win32.cpp"
+)
+
+add_library(imgui STATIC
+    ${imgui_sources} ${imgui_impl_dx12}
+)
+target_include_directories(imgui PUBLIC
+    $<BUILD_INTERFACE:${imgui_source_dir}>
+    $<BUILD_INTERFACE:${imgui_backend_dir}>
+)
+target_link_libraries(imgui PUBLIC
+    d3d12
+    dxgi
+)
 
 set_property(TARGET imgui PROPERTY FOLDER "3rdParty"
 )
