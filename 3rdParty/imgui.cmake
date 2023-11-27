fa6ef604a9c2ba26ae288d84bd8d71d23043ac11
MicroYY <jianhuyy@hotmail.com>
2023-11-27 22:47:55 +0800

    Add OpenGL backend support

--- 3rdParty/imgui.cmake
+++ 3rdParty/imgui.cmake
@@ -8,21 +8,39 @@ set(imgui_backend_dir
 file(GLOB imgui_sources
     CONFIGURE_DEPENDS "${imgui_source_dir}/*.cpp"
 )
-file(GLOB imgui_impl_dx12
-    CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_dx12.cpp"
-    CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_win32.cpp"
+
+add_library(imgui STATIC)
+
+if(BACKEND STREQUAL "dx12")
+    file(GLOB imgui_backend
+        CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_dx12.cpp"
+        CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_win32.cpp"
+    )
+    target_link_libraries(imgui PUBLIC
+        d3d12
+        dxgi
+    )
+elseif(BACKEND STREQUAL "ogl")
+    file(GLOB imgui_backend
+        CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_glfw.cpp"
+        CONFIGURE_DEPENDS "${imgui_backend_dir}/imgui_impl_opengl3.cpp"
+    )
+    target_include_directories(imgui PUBLIC
+        ${imgui_source_dir}/examples/libs/glfw/include
+    )
+    target_link_libraries(imgui PUBLIC
+        opengl32
+        ${imgui_source_dir}/examples/libs/glfw/lib-vc2010-64/glfw3.lib
+    )
+endif()
+
+target_sources(imgui PRIVATE
+    ${imgui_sources} ${imgui_backend}
 )
 
-add_library(imgui STATIC
-    ${imgui_sources} ${imgui_impl_dx12}
-)
 target_include_directories(imgui PUBLIC
     $<BUILD_INTERFACE:${imgui_source_dir}>
     $<BUILD_INTERFACE:${imgui_backend_dir}>
-)
-target_link_libraries(imgui PUBLIC
-    d3d12
-    dxgi
 )
 
 set_property(TARGET imgui PROPERTY FOLDER "3rdParty"
