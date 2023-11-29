c51d5b483f8218828bf7ffa21d046e5394a893e5
Jay Yang <jay.yang@intel.com>
2023-11-29 14:29:38 +0800

    OGL demo usage

--- src/test_ogl.cpp
+++ src/test_ogl.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 import shader;
 
 #pragma comment(lib, "legacy_stdio_definitions.lib")
@@ -17,9 +20,10 @@ const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "layout (location = 1) in vec3 aColor;\n"
 "out vec3 ourColor;\n"
+"uniform vec3 posOffset;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
+"   gl_Position = vec4(aPos + posOffset, 1.0);\n"
 "   ourColor = aColor;\n"
 "}\0";
 
@@ -64,7 +68,8 @@ int main()
         return -1;
     }
 
-    Shader shader("../../src/shaders/shader.vs", "../../src/shaders/shader.fs");
+    Shader shaderGradient("../../src/shaders/shader.vs", "../../src/shaders/shader.fs");
+    Shader shaderTexture("../../src/shaders/texShader.vs", "../../src/shaders/texShader.fs");
 
     // build and compile our shader program
     // ------------------------------------
@@ -111,21 +116,36 @@ int main()
     // ------------------------------------------------------------------
     float vertices1[] = {
         // first triangle
-        -0.9f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-        -0.0f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-        -0.45f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+        -0.9f + 0.55f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+        -0.0f + 0.55f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+        -0.45f + 0.55f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f
     };
     float vertices2[] = {
         // second triangle
          0.0f, -0.5f, 0.0f,  // left
          0.9f, -0.5f, 0.0f,  // right
          0.45f, 0.5f, 0.0f   // top 
+    };    
+
+    float rectangle[] = {
+        0.1f,  0.5f,  0.1f,  0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+        0.1f,  -0.5f, 0.1f,  1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+        -0.9f, -0.5f, 0.1f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.9f, 0.5f,  0.1f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f
     };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+                                              
+    unsigned int VBO[3], VAO[3];
+    glGenVertexArrays(3, VAO); 
+    glGenBuffers(3, VBO);
 
-    unsigned int VBO[2], VAO[2];
-    glGenVertexArrays(2, VAO); 
-    glGenBuffers(2, VBO);
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
+/*----------------------------------------------------*/
     glBindVertexArray(VAO[0]);
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
@@ -133,13 +153,39 @@ int main()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));	// Vertex attributes stay the same
     glEnableVertexAttribArray(1);
+/*----------------------------------------------------*/
 
+/*----------------------------------------------------*/
     glBindVertexArray(VAO[1]);
     glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Vertex attributes stay the same
     glEnableVertexAttribArray(0);
+/*----------------------------------------------------*/
 
+/*----------------------------------------------------*/
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("../../assets/MyImage01.jpg", &width, &height, &nrChannels, 0);
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
+    glBindVertexArray(VAO[2]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
+/*----------------------------------------------------*/
 
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -157,18 +203,29 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        shader.use();
+        shaderGradient.use();
         glBindVertexArray(VAO[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         glUseProgram(shaderProgramBlue);
         float timeValue = glfwGetTime();
         float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         int vertexColorLocation = glGetUniformLocation(shaderProgramBlue, "ourColor");
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        int vertexPos = glGetUniformLocation(shaderProgramBlue, "posOffset");
+        glUniform3f(vertexPos, 0.1f, 0.0f, 0.0f);
+
         // then we draw the second triangle using the data from the second VAO
         glBindVertexArray(VAO[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+        shaderTexture.use();
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO[2]);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -178,8 +235,8 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(2, VAO);
-    glDeleteBuffers(2, VBO);
+    glDeleteVertexArrays(3, VAO);
+    glDeleteBuffers(3, VBO);
     glDeleteProgram(shaderProgramBlue);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
